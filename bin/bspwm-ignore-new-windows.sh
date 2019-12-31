dbb08f18a86bd8cc348115a24e1cacb5c2d21217
Vftdan <vftdan@gmail.com>
2019-12-31 15:57:09 +0300

    Bin (bspwm): fix for nodes in manual mode

--- bin/bspwm-ignore-new-windows.sh
+++ bin/bspwm-ignore-new-windows.sh
@@ -1,7 +1,18 @@
 #! /bin/sh
-bspc subscribe node | grep --line-buffered '^node_manage' | while read line; do \
-	new=$(echo $line | cut -d ' ' -s -f 4)
-	old=$(echo $line | cut -d ' ' -s -f 5)
-	bspc node $new -s $old
-	bspc node $old -f
+last_presel='0x0'
+last_presel_ns='0'
+
+bspc subscribe node | egrep --line-buffered '^node_(manage|presel)' | while read line; do \
+	if ( echo $line | grep -q '^node_presel' ); then \
+		last_presel=$(echo $line | cut -d ' ' -s -f 4)
+		last_presel_ns=$(date +%s%N)
+	else
+		new=$(echo $line | cut -d ' ' -s -f 4)
+		old=$(echo $line | cut -d ' ' -s -f 5)
+		if [ \! \( \( $(( $(date +%s%N) - $last_presel_ns )) -lt 20000000 \) \
+			-a \( "$old" = "$last_presel" \) \) ]; then \
+			bspc node $new -s $old
+			bspc node $old -f
+		fi
+	fi
 done
