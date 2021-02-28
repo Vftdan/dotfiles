48558f3e03ca298f1b4e00680d6a6fad888545e0
Vftdan <vftdan@gmail.com>
2021-02-28 15:25:39 +0100

    Packages: qdbus or qdbus-qt5

--- deployment-scripts/ubuntu-install-packages.sh
+++ deployment-scripts/ubuntu-install-packages.sh
@@ -3,7 +3,8 @@
 sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt update
 sudo apt install cinnamon neovim rofi xscreensaver feh sxiv dunst lxpolkit \
-	tint2 suckless-tools qdbus compton pcmanfm tmux
+	tint2 suckless-tools qtchooser compton pcmanfm tmux
+sudo apt install qdbus || sudo apt install qdbus-qt5
 sudo apt install bspwm sxhkd || \
 	( wget http://archive.ubuntu.com/ubuntu/pool/universe/s/sxhkd/sxhkd_0.5.8-1_amd64.deb; \
 	wget http://archive.ubuntu.com/ubuntu/pool/universe/b/bspwm/bspwm_0.9.3-1_amd64.deb )
