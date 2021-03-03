964b9d874c6f671c3f986072e6ba908cc0f3ecb6
Vftdan <vftdan@gmail.com>
2021-03-03 10:54:33 +0300

    Bspwm: make tint2 semitransparent when a window overlaps with it
    TODO: investigate the layer of rendering of tint2
    Initially, it's below fullscreen window, but later may become above
    E. g. after rotating screen using xrandr

---
+++ bin/bspwm-tint2-compton-transparecy.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+onlyvisible() {
+	while read w rest; do \
+		bspc query -N -n $w'.!hidden' | if read w; then \
+			echo "$w" "$rest"
+		fi
+	done
+}
+
+panelheight=80
+if [ $# -gt 0 ]; then \
+	panelheight=$1
+fi
+bspc subscribe all | \
+		egrep --line-buffered '^desktop_focus|^node_state.*(fullscreen|floating)|^node_flag.*hidden|^node_remove|^pointer_action' | \
+		while read line; do \
+	# "compton-trans -n ..." doesn't terminate 
+	w=$(lsw | grep tint2 | cut -f 1 -d' ')
+	# Sometimes the wrong desktop is used
+	# Hopefully, a small delay will fix it
+	echo "$line" | egrep -q 'desktop_focus' && sleep .1
+	d=$(wmctrl -d | sed -e 's/ \+/\t/g' | egrep '^[0-9]+\s\*' | cut -f 1)
+	# consider using awk
+	miny=$(wmctrl -lG | onlyvisible | sed -e 's/ \+/\t/g' | cut -f 2,4 | \
+			egrep "^$d\\s" | cut -f 2 | sort -n | head -n 1)
+	if [ "$miny" -lt "$panelheight" ]; then \
+		compton-trans -w "$w" -o 35
+	else
+		compton-trans -w "$w" -o 100
+	fi
+done
