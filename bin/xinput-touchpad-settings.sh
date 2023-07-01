9bdfe3229cf5cea1b2e8f7bdfdecb737a62568f6
Vftdan <vftdan@gmail.com>
2023-07-01 21:46:54 +0300

    Bin: touchpad xinput settings script
    For active touchpad use parallely with keyboard

---
+++ bin/xinput-touchpad-settings.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+xinput --set-prop "ELAN1200:00 04F3:309F Touchpad" "libinput Tapping Enabled" 1
+xinput --set-prop "ELAN1200:00 04F3:309F Touchpad" "libinput Scroll Method Enabled" 1 0 0
+xinput --set-prop "ELAN1200:00 04F3:309F Touchpad" "libinput Disable While Typing Enabled" 0
