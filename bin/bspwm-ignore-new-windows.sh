a06fb7ced811fc9671feb553ac5c07073e4aed08
Vftdan <vftdan@gmail.com>
2023-11-29 19:32:17 +0300

    Bspwm: don't prevent new windows from being focused, if they aren't listed in the taskbar or are fullscreen

--- bin/bspwm-ignore-new-windows.sh
+++ bin/bspwm-ignore-new-windows.sh
@@ -15,10 +15,13 @@ bspc subscribe node | egrep --line-buffered '^node_(manage|add|presel)' | while 
 			old="$tmp"
 			# see git@github.com:baskerville/bspwm.git commit 1966104
 		fi
-		if [ \! \( \( $(( $(date +%s%N) - $last_presel_ns )) -lt 20000000 \) \
+		xprop -id "$new" | egrep '^(_NET_WM_STATE\()' | if grep -q '_NET_WM_STATE_SKIP_TASKBAR'; then \
+			continue
+		elif [ \! \( \( $(( $(date +%s%N) - $last_presel_ns )) -lt 20000000 \) \
 			-a \( "$old" = "$last_presel" \) \) ]; then \
 			bspc node $new -s $old
-			[ -z "$(xdetectgrab -knd 1 1)" ] && {
+			[ -z "$(xdetectgrab -knd 1 1)" ] && \
+					! bspc query -N -n "$new.fullscreen" >/dev/null && {
 				bspc node $old -a || \
 				bspc node $old -f
 			}
